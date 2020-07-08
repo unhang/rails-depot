0f0fba9c765fc5f1dbe226e0072f3857e2024616
unhang11@gmail.com <unhang11@gmail.com>
2020-07-08 10:37:54 +0700

    b

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -54,8 +54,7 @@ class ProductTest < ActiveSupport::TestCase
   test "product is not valid without a unique title - i18n" do 
   product = Product.new title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif"
     assert product.invalid?
-    assert_equal [I18n.translate('errors.messages.taken')],
-    product.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
 end
