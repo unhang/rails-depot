051d8497ce3d0ca50b7199c6b979835135769c28
unhang11@gmail.com <unhang11@gmail.com>
2020-07-08 15:56:51 +0700

    D

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'store/index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -52,7 +52,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product is not valid without a unique title - i18n" do 
-  product = Product.new title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif"
+    product = Product.new title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif"
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
